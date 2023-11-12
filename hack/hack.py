6232e9d0159c2d3ae54e6d3891c6d83fc9b016c3
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-11-12 11:06:27 +0000

    Update hack.py

--- hack/hack.py
+++ hack/hack.py
@@ -31,7 +31,6 @@ from redbot.core.utils.chat_formatting import humanize_list
 
 from .utils import get_email_and_password, get_last_dm, loading
 
-
 class Hack(commands.Cog):
     """Le professional hecker."""
 
@@ -67,40 +66,70 @@ class Hack(commands.Cog):
         # Mass editing lol
         message = await ctx.send(f"{loading(0)} Hacking {member.name} now...")
         await asyncio.sleep(2)
+
         try:
-            await message.edit(content=f"{loading(1)} Finding Discord Login...")
+            # Custom Embed for a more visually appealing output
+            embed = discord.Embed(title="Hacking in Progress", color=0x00ff00)
+            embed.set_author(name="Le Professional Hacker", icon_url=self.bot.user.avatar_url)
+            embed.set_thumbnail(url=member.avatar_url)
+
+            # Initial Embed with the progress bar
+            embed.add_field(name="Progress", value=":green_square: :green_square: :green_square: :green_square: :green_square:")
+
+            await message.edit(embed=embed)
             await asyncio.sleep(2)
-            await message.edit(content=f"{loading(2)} Bypassing 2FA...")
+
+            # Update Embed for different stages of hacking
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :green_square: :green_square: :green_square: :green_square:")
+            await message.edit(embed=embed)
+            await asyncio.sleep(2)
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :green_square: :green_square: :green_square:")
+            await message.edit(embed=embed)
             await asyncio.sleep(3)
+
             email, password = await asyncio.to_thread(get_email_and_password, member)
-            await message.edit(
-                content=(
-                    f"{loading(3)} Found login information:\n"
-                    f"**Email**: `{email}`\n"
-                    f"**Password**: `{password}`"
-                )
-            )
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :green_square: :green_square:")
+            embed.add_field(name="Found login information", value=f"**Email**: `{email}`\n**Password**: `{password}`", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(4)
-            await message.edit(content=f"{loading(0)} Fetching user DMs...")
-            await asyncio.sleep(1)
-            last_dm = await asyncio.to_thread(get_last_dm)
-            await message.edit(content=f"{loading(1)} **Last DM**: `{last_dm}`")
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :green_square:")
+            embed.add_field(name="Fetching user DMs", value=f"**Last DM**: `{await asyncio.to_thread(get_last_dm)()}`", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(3)
-            await message.edit(content=f"{loading(2)} Injecting trojan virus into {member}...")
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark:")
+            embed.add_field(name="Injecting trojan virus", value=f"Into {member}...", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(2)
-            await message.edit(content=f"{loading(3)} Virus injected. Finding IP Address...")
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark:")
+            embed.add_field(name="Virus injected", value="Finding IP Address...", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(3)
+
             # A valid IP address must be in the form of x.x.x.x, where x is a number from 0-255.
             ip_address = f"{randint(0, 255)}.{randint(0, 255)}.{randint(0, 255)}.{randint(0, 255)}"
-            await message.edit(content=f"{loading(0)} **IP Address**: `{ip_address}`")
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark:")
+            embed.add_field(name="IP Address", value=f"`{ip_address}`", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(2)
-            await message.edit(content=f"{loading(1)} Selling user data to the government...")
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark:")
+            embed.add_field(name="Selling user data", value="To the government...", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(2)
-            await message.edit(
-                content=f"{loading(2)} Reporting account to Discord for breaking ToS..."
-            )
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark:")
+            embed.add_field(name="Reporting account", value="To Discord for breaking ToS...", inline=False)
+            await message.edit(embed=embed)
             await asyncio.sleep(1)
-            await message.edit(content=f"{commands.context.TICK} Finished hacking {member.name}.")
+
+            embed.set_field_at(0, name="Progress", value=":white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark: :white_check_mark:")
+            embed.add_field(name="Finished hacking", value=f"{member.mention} :white_check_mark:", inline=False)
+            await message.edit(embed=embed)
+
             await ctx.send("The *totally* real and dangerous hack is complete.")
         except discord.NotFound:
             await ctx.send("Process terminated. The hack failed.")
